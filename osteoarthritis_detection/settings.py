2c1c3a0374950e2c969e2bed76d2b247bce38f1f
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2023-10-12 14:14:19 -0400

    Preliminary Skeleton

--- osteoarthritis_detection/settings.py
+++ osteoarthritis_detection/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'osteoarthritis_detection.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -62,6 +62,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                
             ],
         },
     },

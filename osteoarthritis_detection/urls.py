38822b643485c8b112d4b4343cb72705b34550cd
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2023-10-12 18:05:44 -0400

    Preliminary Front End UI Changes

--- osteoarthritis_detection/urls.py
+++ osteoarthritis_detection/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from . import views
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
-]
+]+ static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)

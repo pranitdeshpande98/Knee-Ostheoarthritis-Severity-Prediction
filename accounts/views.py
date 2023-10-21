3325e0616234611db7505428df4b6e97525e9439
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2023-10-20 20:56:13 -0400

    Next set of changes

--- accounts/views.py
+++ accounts/views.py
@@ -222,7 +222,6 @@ def generate_bar_chart(y_pred):
     ax.set_xticks(range(0, 101, 20))
     return fig
 
-import streamlit as st
 def prediction(request):
     y_pred = None
     if request.method == "POST" and request.FILES.get("image"):
@@ -278,7 +277,20 @@ def prediction(request):
             run_id=run_id,
             severity_grade=grade,
         )
-        prediction_run.input_image.save(f"{run_id}_{uploaded_image}", uploaded_image)
+        # Convert the input image to JPEG format
+        input_byte_array = BytesIO()
+        img.save(input_byte_array, format='JPEG')
+        input_byte_array.seek(0)
+
+# Create an InMemoryUploadedFile for the input image
+        input_file = InMemoryUploadedFile(
+            input_byte_array, None, f"{run_id}_input.jpg", 'image/jpeg',
+            input_byte_array.tell(), None
+            )
+
+# Save the input image to your model
+        # Save the input image to the model
+        prediction_run.input_image.save(f"{run_id}_input.jpg", input_file)
         # Convert the heatmap image to bytes
         heatmap_byte_array = BytesIO()
         heatmap_img.save(heatmap_byte_array, format='JPEG')
@@ -296,12 +308,6 @@ def prediction(request):
 #        heatmap_img_path = os.path.join(settings.MEDIA_ROOT, f"{run_id}_heatmap.jpg")
 #        heatmap_img.save(heatmap_img_path)
 
-# Save the input image
-#        input_image_path = os.path.join(settings.MEDIA_ROOT, f"{run_id}_{uploaded_image}")
-#        uploaded_image.seek(0)  # Ensure the file pointer is at the beginning
-#        with open(input_image_path, 'wb') as f:
-#            f.write(uploaded_image.read())
-
         # Generate and save the bar chart image
         bar_chart = generate_bar_chart(y_pred)
         bar_chart_img_path = os.path.join(settings.MEDIA_ROOT, f"analysis_plots/{run_id}_bar_chart.jpg")
@@ -309,6 +315,7 @@ def prediction(request):
 
         prediction_run.bar_chart_analysis.name = f"analysis_plots/{run_id}_bar_chart.jpg"
         prediction_run.save()
+        print(prediction_run.input_image.url)
 
 # Prepare JSON response for displaying results
         response_data = {
@@ -316,8 +323,10 @@ def prediction(request):
             "probability": value,
             "run_id": run_id,
             "heatmap_url": prediction_run.gradcam_heatmap.url,
+            "input_url":  prediction_run.input_image.url,
             "bar_chart_url": prediction_run.bar_chart_analysis.url,
         }
+
         return JsonResponse(response_data)
 
     return render(request, 'inner-page.html')
